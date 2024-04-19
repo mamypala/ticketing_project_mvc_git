fe5cd09c78c3707d9b1585589a2c568ef5677107
MBP <mamypala@hotmail.com>
2024-04-19 18:31:28 +0300

    project create page

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,22 +1,51 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public ProjectController(UserService userService, ProjectService projectService) {
+        this.userService = userService;
+        this.projectService = projectService;
+    }
 
     @GetMapping("/create")
     public String createProject(Model model){
 
         model.addAttribute("project", new ProjectDTO()); // yeni project objesi
 
-        model.addAttribute("managers", );
+        model.addAttribute("managers", userService.findAll());
+
+        model.addAttribute("projects", projectService.findAll());
 
         return "/project/create";
     }
+
+    @PostMapping("/create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectCode}")
+    public String deleteProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.deleteById(projectCode);
+        return "redirect:/project/create";
+    }
+
+
 }
